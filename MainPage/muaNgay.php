46d71236161cc9c03311f35dead31f2e7b8ef516
Windows 10 <minhluu2608@gmail.com>
2022-04-24 15:23:05 +0700

    cap nhat

--- MainPage/muaNgay.php
+++ MainPage/muaNgay.php
@@ -9,13 +9,6 @@
 	
 	if(isset($_SESSION['username'])){
 		if($sl_sp <= $sl_ton){
-			$query = "SELECT so_du from account WHERE username='".$_SESSION['username']."'";	
-			$result = mysqli_query($connect, $query);
-			$data = array();
-			while($row = mysqli_fetch_array($result, 1)){
-				$data[] = $row;
-			}
-			$so_du = $data[0]['so_du'];
 			
 			$query = "SELECT round(`Gia_LK`*(1-`Giam_gia`),0) as Gia_giam FROM `linhkien` WHERE id_LK='".$id_lk."';";	
 			$result = mysqli_query($connect, $query);
@@ -25,37 +18,27 @@
 			}
 			$gia_giam = $data[0]['Gia_giam'];
 			
-			if($so_du >= $gia_giam){
-				$capnhat_sodu = $so_du - $gia_giam;
-				$query = "INSERT INTO `donhang` (`Username_KH`, `Status_DH`, `Ngay_Dat`, `Tong_tien`) 
-					VALUES ('".$_SESSION['username']."', 'Chờ xử lý', sysdate(), '".$gia_giam."')";
-				mysqli_query($connect, $query);
-				
-				$query = "Select max(ID_DH) as max from donhang";	
-				$result = mysqli_query($connect, $query);
-				$data = array();
-				while($row = mysqli_fetch_array($result, 1)){
-					$data[] = $row;
-				}
-				$id_dh = $data[0]['max'];
-				
-				$query = "INSERT INTO `chitietDH` (`ID_DH`, `ID_LK`, `So_luong`, `Don_gia`) 
-					VALUES ('".$id_dh."', '".$id_lk."', '".$sl_sp."', '".$gia_giam."')";
-				mysqli_query($connect,$query);
-				
-				$query = "update account set so_du='".$capnhat_sodu."' WHERE username='".$_SESSION['username']."'";	
-				mysqli_query($connect, $query);
-				
-				$query = "update linhkien set so_luong='".($sl_ton - $sl_sp)."' WHERE id_lk='".$id_lk."'";	
-				mysqli_query($connect, $query);
-				
-				$_SESSION['buy_status'] = "Đã mua thành công.";
-				header("Location: index.php");
+			$query = "Select max(ID_DH) as max from donhang";	
+			$result = mysqli_query($connect, $query);
+			$data = array();
+			while($row = mysqli_fetch_array($result, 1)){
+				$data[] = $row;
 			}
-			else{
-				$_SESSION['buy_status'] = "Số tiền trong tài khoản không đủ để mua hàng";
-				header("Location: chitiet_SP.php?id_lk=".$id_lk."&loai_lk=".$loai_lk);
-			}
+			$id_dh = $data[0]['max'] + 1;
+
+			$query = "INSERT INTO `donhang` (ID_DH, `ID_User_KH`, Diachi_DH, `Status_DH`, `Ngay_Dat`, `Tong_tien`) 
+				VALUES ($id_dh,'".$_SESSION['id_user']."', 'Diachi','Chờ xử lý', sysdate(), '".$gia_giam."')";
+			mysqli_query($connect, $query);
+				
+			$query = "INSERT INTO `chitietDH` (`ID_DH`, `ID_LK`, `So_luong`, `Don_gia`) 
+				VALUES ('".$id_dh."', '".$id_lk."', '".$sl_sp."', '".$gia_giam."')";
+			mysqli_query($connect,$query);
+						
+			$query = "update linhkien set so_luong='".($sl_ton - $sl_sp)."' WHERE id_lk='".$id_lk."'";	
+			mysqli_query($connect, $query);
+				
+			$_SESSION['buy_status'] = "Đã mua thành công.";
+			header("Location: index.php");
 		}
 		else{
 			$_SESSION['buy_status'] = "Số lượng vượt quá số hàng tồn kho";
@@ -66,5 +49,4 @@
 		$_SESSION['buy_status'] = "Hãy đăng nhập trước khi thực hiện mua hàng";
 		header("Location: chitiet_SP.php?id_lk=".$id_lk."&loai_lk=".$loai_lk);
 	}
-	
 ?>
