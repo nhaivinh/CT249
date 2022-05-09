9fcc9e38a3f80b2191205ff7edaaad95a4279d0e
nhaivinh <nhaivinh2000@gmail.com>
2022-05-09 20:15:43 +0700

    tao index.php admin doi ten mainpage.php admin

--- AdminPage/index.php
+++ AdminPage/index.php
@@ -2,40 +2,13 @@
 	require_once("../DBprocess.php");
 	require_once('../MainPage/HTMLprocess.php');
 	require_once("HTMLprocess.php");
-	
-	if(isset($_SESSION['username'])){
-		$connect = connectDB();
-		$query = "Select Quyen_han from Account where username = '".$_SESSION['username']."'";
-		$result = mysqli_query($connect, $query);
-		$data = array();
-		while($row = mysqli_fetch_array($result, 1)){
-			$data[] = $row;
-		}
-		$privilege = $data[0]['Quyen_han'];
-		if(isset($_SESSION['privilege']))
-			if(strcmp($_SESSION['privilege'],"Owner") == 0 || strcmp($_SESSION['privilege'],"Senior Staff") == 0 || strcmp($_SESSION['privilege'],"Staff") == 0){
-				if($privilege != $_SESSION['privilege']){
-					header("Location: ../MainPage/index.php");
-				}
-			}
-			else {
-				header("Location: ../MainPage/index.php");
-			}
-		else{
-			header("Location: ../MainPage/index.php");
-		}
-	}
-	else{
-		header("Location: ../MainPage/index.php");
-	}
-	
 ?>
 <!doctype html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="./CSS/index.css" />
-	<title>Shop linh kiện Demo</title>
+	<title>Login Admin</title>
 </head>
 <body>
 	<div class="page_Header">
@@ -67,9 +40,15 @@
 	<br>
 	<div class="admin_content">
 		<div class="content">
-<?php
-			ShowThongBao();
-?>
+            <div class="login-container" id="hidden-popup">
+                <div class="login-popup" id="hidden-login">
+                    <form action="" method="POST" class="login-input-container">
+                        <input type="username" class="login-input" name="username" pattern="[a-zA-Z0-9]+$" placeholder="Tên người dùng">
+                        <input type="password" class="login-input" name="password" placeholder="Mật khẩu">
+                        <input type="submit" value="ĐĂNG NHẬP" class="login-login-btn">
+                    </form>
+                </div>
+            </div>
 		</div>
 	</div>
 </body>
---
+++ AdminPage/mainpage.php
@@ -0,0 +1,76 @@
+<?php
+	require_once("../DBprocess.php");
+	require_once('../MainPage/HTMLprocess.php');
+	require_once("HTMLprocess.php");
+	
+	if(isset($_SESSION['username'])){
+		$connect = connectDB();
+		$query = "Select Quyen_han from Account where username = '".$_SESSION['username']."'";
+		$result = mysqli_query($connect, $query);
+		$data = array();
+		while($row = mysqli_fetch_array($result, 1)){
+			$data[] = $row;
+		}
+		$privilege = $data[0]['Quyen_han'];
+		if(isset($_SESSION['privilege']))
+			if(strcmp($_SESSION['privilege'],"Owner") == 0 || strcmp($_SESSION['privilege'],"Senior Staff") == 0 || strcmp($_SESSION['privilege'],"Staff") == 0){
+				if($privilege != $_SESSION['privilege']){
+					header("Location: ../MainPage/mainpage.php");
+				}
+			}
+			else {
+				header("Location: ../MainPage/mainpage.php");
+			}
+		else{
+			header("Location: ../MainPage/mainpage.php");
+		}
+	}
+	else{
+		header("Location: ../MainPage/mainpage.php");
+	}
+	
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="./CSS/mainpage.css" />
+	<title>Shop linh kiện Demo</title>
+</head>
+<body>
+	<div class="page_Header">
+		<div class="Account">
+			<ul>
+				<li style="border-left-width: 0px;width:500px;"><a href="../MainPage/mainpage.php">Shop linh kiện DEMO - bán tất cả loại linh kiện máy tính</a></li>
+<?php
+				if(isset($_SESSION['username'])){
+?>
+					<li id="Welcome">
+						<p>
+							<?php echo "Welcome ".$_SESSION['privilege']." ".$_SESSION['username']; ?> 
+						</p>
+					</li>
+					<li><a href ="../MainPage/Logout.php">Đăng xuất</a></li>
+<?php
+				}
+?>
+			</ul>
+		</div>
+	</div>
+	<div class="nav_bar">
+		<a href="mainpage.php">Về trang chủ</a>
+		<a href="QL_Account.php">Xem thông tin Account</a>
+		<a href="QL_SP.php">Quản lí hàng hóa</a>
+		<a href="QL_DH.php">Quản lí đơn hàng</a>
+	</div>
+	<br>
+	<br>
+	<div class="admin_content">
+		<div class="content">
+<?php
+			ShowThongBao();
+?>
+		</div>
+	</div>
+</body>
+</html>
